0b6cfd052d8a7daff585af6f3cd3c2166c72a292
김우현 <rladngus133@naver.com>
2024-06-23 22:20:39 +0900

    build/파이어베이스 설치 및 세팅

---
+++ src/firebaseInit.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALnMCMvRZEPwrwlPfh4RT9CEyz1FQ2vNM",
+  authDomain: "savelinkdatabase.firebaseapp.com",
+  projectId: "savelinkdatabase",
+  storageBucket: "savelinkdatabase.appspot.com",
+  messagingSenderId: "947136810929",
+  appId: "1:947136810929:web:0bec98b0580da78f91b790",
+  measurementId: "G-KREKT98CP4",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
